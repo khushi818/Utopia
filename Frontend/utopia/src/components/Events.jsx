3de79967cb508823829426833a0196aa9e6ad36e
khushi <khushishukla5890@gmail.com>
2023-02-04 23:30:28 +0530

    integration

--- Frontend/utopia/src/components/Events.jsx
+++ Frontend/utopia/src/components/Events.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Events = () => {
+    const [room, setRoom] = useState([])
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/room/', {
+            'method': 'GET',
+            header: {
+                'Content-Type': 'application/Json',
+            }
+        })
+            .then(res => res.json())
+            .then(res => setRoom(res))
+            .catch(error => console.log(error))
+    }, [])
     return (
         <div className='flex items-center justify-center flex-col'>
-            events
+            {room.map((r) => {
+                return <h1>{r.name}</h1>
+            })}
         </div>
     )
 }
