f7cb81a1a5db705fa098111f88fa077572896582
Khushi Shukla <khushishukla5890@gmail.com>
2023-03-15 10:29:38 +0530

    gitignore

--- Backend/utopia/static/js/stream.js
+++ Backend/utopia/static/js/stream.js
@@ -1,7 +1,6 @@
 const App_Id = "50aa357a11604d798b12088f413a4efa"
 const Channel = 'main'
-const Token = "007eJxTYOC1Y9jcp7dEauf6GENekcy/n0RW7L2UVcnndSr1wNmVvVcVGEwNEhONTc0TDQ3NDExSzC0tkgyNDCws0kwMjRNNUtMSvx7kSmkIZGQ4peLMzMgAgSA+C0NuYmYeAwMA7ececA=="
-
+const Token = "007eJxTYIi8L3//8uz1ChsEvv4OmLpSL2S9xYO85v39iw23nq5R4OlWYDA1SEw0NjVPNDQ0MzBJMbe0SDI0MrCwSDMxNE40SU1LjD8tkNIQyMjg/WsWMyMDBIL4LAy5iZl5DAwAm9IgJQ=="
 let UID;
 
 const client = AgoraRTC.createClient({mode:'rtc',codec:'vp8'})
@@ -9,55 +8,87 @@ const client = AgoraRTC.createClient({mode:'rtc',codec:'vp8'})
 let localTracks =[]
 let remoteUsers = {}
 
-let joinAndDisplayLocalStream = async () =>{
+let joinAndDisplayLocalStream = async () => {
+//    document.getElementById('room-name').innerText = CHANNEL
 
-    client.on("user-published", handleUserJoined) 
+    client.on('user-published', handleUserJoined)
+   client.on('user-left', handleUserLeft)
 
-    UID = await client.join(App_Id,Channel,Token,UID)
-
+    try{
+        UID = await client.join(App_Id, Channel , Token , UID)
+    }catch(error){
+        console.error(error)
+        window.open('/join', '_self')
+    }
+    
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
 
-    console.log(localTracks[0])
-     
-    player = `<div class = "video-container" id = "user-container-${UID}">
-        <div class= "username-wrapper"><span class = "user-name">My Name</span></div>
-        <div class = "video-player" id = "user-${UID}"></div>
-        </div>  `
+    //let member = await createMember()
 
-    document.getElementById('video-streams').insertAdjacentHTML('beforeend',player)
-     
-    localTracks[1].play(`user-${UID}`)
-
-    await client.publish(localTracks[0],localTracks[1])
-}
-
-let handleUserJoined = async (user,mediaType) =>{
-    remoteUsers[user.uid] = user
+    let player = `<div  class="video-container" id="user-container-${UID}">
+                     <div class="video-player" id="user-${UID}"></div>
+                     <div class="username-wrapper"><span class="user-name">Name</span></div>
+                  </div>`
     
-    await client.subscribe(user,mediaType)
-    if(mediaType === 'video')
-    {
-      let player = document.getElementById(`user-container-${user.uid}`)
-      console.log(player) 
-    //    if(player != null)
-    //    {
-    //      player.remove()
-    //    }
-         player = `<div class = "video-container" id = "user-container-${user.uid}">
-        <div class= "username-wrapper"><span class = "user-name">My Name</span></div>
-        <div class = "video-player" id = "user-${user.uid}"></div>
-        </div>  `
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+    localTracks[1].play(`user-${UID}`)
+    await client.publish([localTracks[0], localTracks[1]])
+}
 
-    document.getElementById('video-streams').appendChild(player)
-     
-    user.videoTrack.play()
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.uid] = user
+    await client.subscribe(user, mediaType)
+
+    if (mediaType === 'video'){
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if (player != null){
+            player.remove()
+        }
+
+        //let member = await getMember(user)
+
+        player = `<div  class="video-container" id="user-container-${user.uid}">
+            <div class="video-player" id="user-${user.uid}"></div>
+            <div class="username-wrapper"><span class="user-name">Name</span></div>
+        </div>`
+
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+        user.videoTrack.play(`user-${user.uid}`)
     }
 
-    if(mediaType ===  'audio')
-    {
-        user.audioTrack.play();
+    if (mediaType === 'audio'){
+        user.audioTrack.play()
     }
 }
+
+let handleUserLeft = async(user) =>{
+    delete remoteUsers[user.uid]
+    document.getElementById(`user-container-${user.uid}`).remove()
+}
+
+let leaveAndRemoveLocalStream = async() =>{
+   for(let i = 0 ; i < localTracks.length ; i++)
+   {
+    localTracks[i].stop()
+    localTracks[i].close()
+   }
+
+   await client.leave()
+   window.open('/join','_self')
+}
+
+let toggleCamera = async (e) =>{
+    if(localTracks[1].muted){
+        await localTracks[1].setMuted(false)
+    }
+    else{
+        await localTracks[1].setMuted(false)
+    }
+    console.log("stream btn")
+}
+
+document.getElementById('leave-button').addEventListener('click', leaveAndRemoveLocalStream)
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
 
 joinAndDisplayLocalStream()
 console.log("stream connected")
--- Backend/utopia/video_call/urls.py
+++ Backend/utopia/video_call/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('utopia/topic',views.lobby),
+    path('join/',views.lobby),
     path('space/',views.space)
 ]
